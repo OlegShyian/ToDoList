bb7749c632877f2a6f9d8ad6d994278927284301
Oleg Shyian <o.shyian@4irelabs.com>
2022-11-26 05:10:19 +0200

    Add to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+interface ITask {
+  task: string;
+  checked: boolean;
+  id: number;
+}
 
 function App() {
+  const [tasks, setTasks] = useState([] as Array<ITask>);
+  const [firstNew, setNewFirst] = useState(true);
+  const [isOnTop, setIsOnTop] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      task: { value: string };
+    };
+    const task = target.task.value;
+    setTasks((current) => {
+      if (firstNew) {
+        return [{ task, checked: false, id: Date.now() }, ...current];
+      } else {
+        return [...current, { task, checked: false, id: Date.now() }];
+      }
+    });
+    e.currentTarget.reset();
+  };
+
+  const handleChecked = (id: number) => {
+    setTasks((current) =>
+      current.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task,
+      ),
+    );
+  };
+
+  const handleTaskRemove = (id: number) => {
+    setTasks((current) => current.filter((task) => task.id !== id));
+  };
+
+  const handleSort = () => {
+    setTasks([...tasks.reverse()]);
+    setNewFirst((current) => !current);
+  };
+
+  const handleSetCheckedOnTop = () => {
+    setIsOnTop((current) => !current);
+    setTasks((current) => {
+      const { checked, unChecked } = current.reduce(
+        (acc, task) =>
+          task.checked
+            ? { ...acc, checked: [...acc.checked, task] }
+            : { ...acc, unChecked: [...acc.unChecked, task] },
+        { checked: [] as ITask[], unChecked: [] as ITask[] },
+      );
+      return [...checked, ...unChecked];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '30px' }}>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='put task' name='task' />
+        <button type='submit'>add</button>
+      </form>
+      <button
+        style={{ marginRight: '10px' }}
+        onClick={handleSort}
+      >{`New first: ${firstNew}`}</button>
+      <button onClick={handleSetCheckedOnTop}>Checked on top</button>
+      {tasks.map(({ task, checked, id }) => (
+        <div key={id} style={{ padding: '5px 0' }}>
+          <input
+            type='checkbox'
+            checked={checked}
+            onChange={() => {
+              handleChecked(id);
+            }}
+          />
+          <span>{task}</span>
+          <button onClick={() => handleTaskRemove(id)}>remove</button>
+        </div>
+      ))}
     </div>
   );
 }
